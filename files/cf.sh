fc6373a61219e41b65a0be76affc22045a8021d1
rwrtx <152061298+rwrtx@users.noreply.github.com>
2024-05-07 23:53:49 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -10,13 +10,12 @@ apt install jq curl -y
 rm -rf /root/xray/scdomain
 mkdir -p /root/xray
 clear
-echo ""
-echo ""
-echo ""
-#sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
+#sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 read -rp "Input Domain Name. Example ( CONTOH : ngewe-gaya69 ) Bebas Mau Isi Apa: " -e sub
+subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 DOMAIN=r32wrtxtunneling.site
-SUB_DOMAIN=${sub}.r32wrtxtunneling.site
+SUB_DOMAIN=pro-${sub}.r32wrtxtunneling.site
+NS_DOMAIN=dns-${subsl}.r32wrtxtunneling.site
 CF_ID=amandafitrizharifa009@gmail.com
 CF_KEY=3c45413104a3c39f1db9dff20acb3a99d7610
 set -euo pipefail
@@ -42,13 +41,38 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
 -H "X-Auth-Key: ${CF_KEY}" \
 -H "Content-Type: application/json" \
 --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "Host NS : $NS_DOMAIN"
+echo "IP=$SUB_DOMAIN" > /var/lib/SIJA/ipvps.conf
 echo "$SUB_DOMAIN" > /root/domain
-echo "$SUB_DOMAIN" > /root/scdomain
+echo "$NS_DOMAIN" > /root/nsdomain
 echo "$SUB_DOMAIN" > /etc/xray/domain
 echo "$SUB_DOMAIN" > /etc/v2ray/domain
-echo "$SUB_DOMAIN" > /etc/xray/scdomain
-echo "IP=$SUB_DOMAIN" > /var/lib/kyt/ipvps.conf
-echo "Subdomain kamu adalah : $SUB_DOMAIN
+echo "$SUB_DOMAIN" > /root/scdomain
+echo "$SUB_DOMAIN" > /root/xray/scdomain
+echo "$NS_DOMAIN" >/etc/xray/dns
+echo -e "Done Record Domain= $SUB_DOMAIN"
+echo -e "Done Record NSDomain= $NS_DOMAIN"
 rm -rf cf
-sleep 2
+sleep 1
